d9a62bbbfc4b6d3403e5345181a0e2a771e0858d
kade514 <prometheus146@gmail.com>
2015-12-12 11:22:00 +0800

    [Michael Vatskalis] - initial player movement done.

---
+++ Project Folder/LudumDareGame/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    Rigidbody thisRB;
+    Vector3 mousePosition;
+	// Use this for initialization
+	void Start ()
+    {
+        thisRB = this.GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        this.transform.LookAt(new Vector3(mousePosition.x,mousePosition.y,0.0f));
+
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("Hi!");
+            thisRB.AddForce(transform.forward * 100f);
+        } 
+	}
+}
