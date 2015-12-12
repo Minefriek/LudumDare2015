842184dd25fbc8d88bd643c466aa172d18d74e6c
kade514 <prometheus146@gmail.com>
2015-12-12 09:57:11 +0800

    [Michael Vatskalis] - Pushing project folder to git

---
+++ Project Folder/LudumDareGame/Assets/Resources/Scripts/ButtonScripts/SetListenerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SetListenerScript : MonoBehaviour {
+
+    public string ManagerFunctionName;
+    public GameObject manager;
+
+    // Use this for initialization
+    void Start()
+    {
+        manager = GameObject.Find("_MANAGER");
+    }
+
+    public void DoActionFromManager()
+    {
+        manager.SendMessage(ManagerFunctionName);
+    }
+}
---
+++ Project Folder/LudumDareGame/Assets/Resources/Scripts/GameLogic/ButtonManager.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour 
+{
+    public static ButtonManager Instance;
+
+    public GameObject mainMenu;
+    public GameObject PauseMenu;
+
+    private enum GameState 
+   {
+       EPlaying, 
+       EWon, 
+       ELost, 
+       EUnknown,
+   };
+
+    GameState state;
+
+    void Awake()
+    {
+        state = GameState.EPlaying;
+
+        if (Instance)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+    }
+
+    void OnLevelWasLoaded(int LevelID)
+    {
+        if (LevelID != 0)
+        {
+            PauseMenu = GameObject.Find("PauseMenu");
+        }
+    }
+
+    void Update()
+    {
+        GameStateChecking();
+
+        if (PauseMenu != null && Input.GetButtonDown("Exit"))
+        {
+            foreach (Transform child in PauseMenu.transform)
+            {
+                child.gameObject.SetActive(!child.gameObject.activeSelf);
+            }
+        }
+    }
+
+    void GameStateChecking()
+    {
+        switch (state)
+        {
+            case GameState.EPlaying:
+                Debug.Log("Playing!");
+                break;
+            case GameState.EWon:
+                Debug.Log("Won!");
+                state = GameState.EPlaying;
+                Application.LoadLevel(0);
+                break;
+            case GameState.ELost:
+                Debug.Log("Lost!");
+                state = GameState.EPlaying;
+                Application.LoadLevel(0);
+                break;
+        }
+    }
+
+	void StartButton()
+    {
+        //state = GameState.EPlaying;
+        Application.LoadLevel(1);
+    }
+
+    void OptionsButton()
+    {
+        mainMenu = GameObject.Find("MainMenu");
+        foreach(Transform child in mainMenu.transform)
+        {
+            Debug.Log(child.name);
+        }
+
+    }
+
+    void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    void ReturnToMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    void ReturnToGame()
+    {
+        foreach (Transform child in PauseMenu.transform)
+        {
+            child.gameObject.SetActive(!child.gameObject.activeSelf);
+        }
+    }
+    void Win()
+    {
+        state = GameState.EWon;
+    }
+
+    void Lose()
+    {
+        state = GameState.ELost;
+    }
+}
